8a7e0efe82701dd485f69914a5e66def25eb73c9
aldlss <ayaldlss@gmail.com>
2024-05-04 14:28:04 +0800

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({
                     }}
                 />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
