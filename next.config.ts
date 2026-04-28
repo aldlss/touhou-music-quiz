5a6dbdcf6ef556b6d7313ef7ea1d038f4d51a3c4
aldlss <ayaldlss@gmail.com>
2026-04-28 15:44:22 +0800

    perf: 引入 React Compiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["ogg-opus-decoder"],
+  reactCompiler: true,
   turbopack: {
     rules: {
       "*.md": {
